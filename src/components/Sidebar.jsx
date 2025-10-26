d5db3b3c6ae2590193a4a64b3b3bb046bae28951
Shekinah Adeogo <adeogoshekinah@gmail.com>
2025-10-26 18:20:00 +0100

    feat: update Navbar and Sidebar layout for improved positioning and responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,9 @@
 const Sidebar = () => {
-  return <aside className='w-64 bg-gray-100 p-4'>Sidebar</aside>;
+  return (
+    <aside className='fixed top-[64px] md:top-[70px] left-0 h-[calc(100vh-180.5px)] md:h-[calc(100vh-150px)] w-64 bg-gray-50 dark:bg-gray-800 text-gray-800 dark:text-gray-100 p-4 transition'>
+      Sidebar
+    </aside>
+  );
 };
 
 export default Sidebar;
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -6,9 +6,9 @@ const DashboardLayout = () => {
   return (
     <div>
       <Navbar />
-      <div className='flex'>
+      <div className='flex pt-[70px]'>
         <Sidebar />
-        <div className='flex-1 p-6 min-w-0'>
+        <div className='flex-1 p-6 min-w-0 ml-64'>
           <Outlet />
         </div>
       </div>
