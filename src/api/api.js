9eb3c6427492407cc1ecf5d3096307f46ae502ec
Shekinah Adeogo <adeogoshekinah@gmail.com>
2025-10-28 23:59:51 +0100

    feat: add response interceptor to handle request timeouts in API module

--- src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://www.themealdb.com/api/json/v1/1',
-  timeout: 10000, // Aborts the request if no response is received within this time, to prevent the app from hanging indefinitely (axios-specific)
-});
-
-export default api;
