0f24506c1f7219f37eaf6c1673b43e01c3277b60
Shekinah Adeogo <adeogoshekinah@gmail.com>
2025-10-30 14:09:54 +0100

    feat: display spinner when image is loading, and display fallback image onError

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,38 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Card = ({ image, title, description, to }) => {
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [imgSrc, setImgSrc] = useState(image);
+
+  const handleError = () => {
+    setImgSrc('/login-image.jpg'); // fallback if image from API is unavailable
+  };
+
   return (
     <Link to={to} className='block'>
       <div className='bg-sky-50 dark:bg-gray-800 shadow rounded-2xl overflow-hidden transition hover:shadow-lg dark:hover:bg-gray-950'>
-        <img
-          src={image}
-          alt={title}
-          className='w-full h-40 md:h-32 lg:h-48 object-cover'
-        />
+        <div className='relative w-full h-48 md:h-32 lg:h-48'>
+          {/* Image */}
+          <img
+            src={imgSrc}
+            alt={title}
+            onLoad={() => setHasLoaded(true)}
+            onError={handleError}
+            className={`w-full h-full object-cover transition-opacity duration-500 ${
+              hasLoaded ? 'opacity-100' : 'opacity-0'
+            }`}
+          />
+
+          {/* Spinner placeholder */}
+          {!hasLoaded && (
+            <div className='absolute inset-0 flex items-center justify-center bg-sky-100 dark:bg-gray-900'>
+              <div className='w-6 h-6 border-2 border-blue-500 border-t-transparent rounded-full animate-spin'></div>
+            </div>
+          )}
+        </div>
+
         <div className='p-4'>
           <h3 className='text-lg font-semibold mb-2 line-clamp-1 text-gray-900 dark:text-gray-100'>
             {title}
