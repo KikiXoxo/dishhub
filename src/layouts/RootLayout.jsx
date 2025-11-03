7df2683e128e0fc1a2446f1bfda02656f9ae6c2d
Shekinah Adeogo <adeogoshekinah@gmail.com>
2025-11-03 14:08:35 +0100

    feat: add scroll to top functionality when navigating between routes

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import ScrollToTop from '../components/ScrollToTop';
 
 const RootLayout = () => {
   return (
     <div className='min-h-screen grid grid-rows-[1fr_auto]'>
       <main className='h-full'>
+        <ScrollToTop />
         <Outlet />
       </main>
       <Footer />
